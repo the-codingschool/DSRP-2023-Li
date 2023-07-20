66c406cbf269ee0740ecb214e10a3dc8119f0e6c
Harmony Ho <hamsterrnr@gmail.com>
2023-07-20 14:50:21 -0500

    checkpoint 1 commit

---
+++ Harmony/data_exploration.R
@@ -0,0 +1,14 @@
+# load in libraries 
+library("dplyr") 
+
+# read in CSV 
+db_large <- read.csv("../data/DatabankWide Dataset - Data.csv")
+
+# filter needed columns 
+# db_small <- select(db_large, )
+
+# group by income level 
+# dependent: numerical/financial literacy 
+# use machine learning for correlation 
+
+# RQ: To what extent does national income level influence improvement in financial literacy over time? 
