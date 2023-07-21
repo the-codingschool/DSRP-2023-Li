80306c20e07c904ab215620f59d301bdbd5859da
faija onjila <onjilaf@gmail.com>
2023-07-21 13:31:21 -0400

    cleaningdata

--- Faija /scripts/cleaningData.R
+++ Faija /scripts/cleaningData.R
@@ -14,17 +14,14 @@ datasm <- select(data, Region, Year, `Country name`,
             `Withdrew money from a financial institution account 2 or more times a month (% who had withdrawn money, age 15+)`,
             `Store money using a financial institution (% age 15+)`,`Store money using a financial institution (% with a financial institution account, age 15+)`) 
  
-datasm
+noNAs <- na.omit(datasm)
 
-rename(data, Country = 'Country name')
+rename(datasm, Country = 'Country name')
+rename(datasm, Owns_debit_or_credit_card = 'Owns a debit or credit card (% age 15+)')
+rename(datasm, Used_debit_or_creditcard =  'Used a debit or credit card (% age 15+)')
+rename(datasm, made_a_deposit = 'Made a deposit (% with a financial institution account, age 15+)')
 
 
-
-filter(datasm, is.na("Paid off all credit card balances in full by their due date (% who used a credit card, age 15+)"))
-filter(datasm, is.na("Used a debit or credit card (% age 15+)"))
-
-noNAs <- na.omit(data)
-noNAs
 
 library(ggplot2)
 
