3d6e22855fd63da832df1945c0a5325d6aee06c7
faija onjila <onjilaf@gmail.com>
2023-07-22 23:44:15 -0400

    Cleaning and plotting micro world data

---
+++ Faija/scripts/microworld.R
@@ -0,0 +1,64 @@
+microworld <- readRDS("~/Documents/ResearchProject-Li/data/micro_world_139countries.RDS")
+View(microworld)
+
+library(dplyr)
+
+## What is the relationship between different regions and account ownership
+
+## Selecting columns related to research question
+smallworld <- select(microworld, economy, account, account_mob, fin2, fin4, fin7, fin8, fin8b,
+                     fin9, fin9a, fin10,fin10a, fin10b, regionwb)
+
+## getting rid of NAs
+Newsmallworld <- na.omit(smallworld)
+
+## renaming columns related to research question
+rename(Newsmallworld, country = "economy") 
+rename(Newsmallworld, has_credit_card = "fin7") 
+rename(Newsmallworld, has_debitcard = "fin2") 
+rename(Newsmallworld, used_debitcard = "fin4")
+rename(Newsmallworld, paid_creditbalace_infull = "fin8b")
+rename(Newsmallworld, made_deposit = "fin9") 
+rename(Newsmallworld, makes_deposit_2pluspermonth = "fin9a") 
+rename(Newsmallworld, withdrew = "fin10") 
+rename(Newsmallworld, withdrew_2plusamonth = "fin10a") 
+rename(Newsmallworld, storemoneyinaccount = "fin10b") 
+
+## Filtering by region
+smallworldSouthAsia <- filter(Newsmallworld, Newsmallworld$regionwb == "South Asia")
+smallworldEAsia_Pacific <- filter(Newsmallworld, Newsmallworld$regionwb  == "East Asia & Pacific (excluding high income)") 
+smallworldEurope_CAsia <- filter(Newsmallworld, Newsmallworld$regionwb  == "Europe & Central Asia (excluding high income)") 
+smallworldHighIncome <- filter(Newsmallworld, Newsmallworld$regionwb == "High income") 
+smallworldLatin_Caribbean <- filter(Newsmallworld, Newsmallworld$regionwb  == "Latin America & Caribbean (excluding high income)") 
+smallworldMiddleEast_NAfrica <- filter(Newsmallworld, Newsmallworld$regionwb  == "Middle East & North Africa (excluding high income)") 
+smallworldSSAfrica <- filter(Newsmallworld, Newsmallworld$regionwb  == "Sub-Saharan Africa (excluding high income)") 
+
+library(ggplot2)
+
+ggplot(data = smallworldSouthAsia, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "darkgreen" ) 
+
+ggplot(data = smallworldHighIncome, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "lightblue" ) 
+
+ggplot(data = smallworldSSAfrica, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "red" ) 
+
+ggplot(data = smallworldLatin_Caribbean, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "violet" ) 
+
+ggplot(data = smallworldMiddleEast_NAfrica, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "darkblue" ) 
+
+ggplot(data = smallworldEurope_CAsia, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "grey" ) 
+
+ggplot(data = smallworldEAsia_Pacific, aes( x = economy , y= fin7)) +
+  geom_bar(stat = "summary",
+           fun = "mean", fill = "orange" ) 
