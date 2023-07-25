17f3c2bbf9a9b5cf2802a18fe2482d0c7d08c0b1
choprarjun <choprarjun.14@gmail.com>
2023-07-25 12:00:17 -0700

    Created this plot with more data exploration

--- Arjun/exploration.R
+++ Arjun/exploration.R
@@ -1,5 +1,11 @@
 library(readr)
+library(ggplot2)
 library(dplyr)
+library(tidyr)
+library(janitor)
+library(reshape2)
+library(rsample)
+library(parsnip)
 data <- readRDS("data/micro_world_139countries.RDS")
 View(data)
 summary(data)
@@ -7,5 +13,19 @@ new_data <- select(data, economy, economycode, regionwb, pop_adult, wpid_random,
                    emp_in, urbanicity_f2f, account, account_fin,
                    account_mob, fin2, fin11_1, fin11a, fin11b, fin11c, fin11d, fin11e, fin11f, fin11g,
                    fin11h)
-data_clean <- na.omit(new_data)
+data_plot <- select( data, educ, fin5, inc_q )
 
+data_plot <- na.omit(data_plot)
+
+data_plot_Cors <- data_plot |>
+  cor() |>
+  melt() |>
+  as.data.frame()
+
+ggplot(data_plot_Cors, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "red", high = "blue", mid = "white",
+                       midpoint = 0)
+data_plot_Cors
+ggplot(data_plot, aes(x= inc_q , y= fin5)) +
+  geom_violin() + labs(title = "Income vs. Internet Access")
