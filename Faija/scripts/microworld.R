6ca1d0ff542d249963b32fcba59061fcb5f12a54
faija onjila <onjilaf@gmail.com>
2023-07-25 12:38:53 -0400

    micro world plot

--- Faija/scripts/microworld.R
+++ Faija/scripts/microworld.R
@@ -33,32 +33,42 @@ smallworldLatin_Caribbean <- filter(Newsmallworld, Newsmallworld$regionwb  == "L
 smallworldMiddleEast_NAfrica <- filter(Newsmallworld, Newsmallworld$regionwb  == "Middle East & North Africa (excluding high income)") 
 smallworldSSAfrica <- filter(Newsmallworld, Newsmallworld$regionwb  == "Sub-Saharan Africa (excluding high income)") 
 
+has_credit_card <- filter(Newsmallworld, )
+
+
 library(ggplot2)
 
 ggplot(data = smallworldSouthAsia, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "darkgreen" ) 
+           fun = "mean", fill = "darkgreen" ) + 
+  labs(x = "Country", y = "Owns Credit Card" , title = "What % of the South Asia Region owns a credit card")
 
 ggplot(data = smallworldHighIncome, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "lightblue" ) 
+           fun = "mean", fill = "lightblue" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the High Income Region owns a credit card")
 
 ggplot(data = smallworldSSAfrica, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "red" ) 
+           fun = "mean", fill = "red" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the Sub-Saharan Africa Region owns a credit card")
 
 ggplot(data = smallworldLatin_Caribbean, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "violet" ) 
-
+           fun = "mean", fill = "violet" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the Latin America and Caribbean Region owns a credit card")
+  
 ggplot(data = smallworldMiddleEast_NAfrica, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "darkblue" ) 
-
+           fun = "mean", fill = "darkblue" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the Middle East and North African Region owns a credit card")
+  
 ggplot(data = smallworldEurope_CAsia, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "grey" ) 
+           fun = "mean", fill = "grey" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the Europe and Central Asian Region owns a credit card")
 
 ggplot(data = smallworldEAsia_Pacific, aes( x = economy , y= fin7)) +
   geom_bar(stat = "summary",
-           fun = "mean", fill = "orange" ) 
+           fun = "mean", fill = "orange" ) +
+  labs(x = "Country", y = "Owns Credit Card", title = "What % of the East Asia and Pacific Region owns a credit card")
