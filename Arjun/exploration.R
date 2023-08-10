ad01aa4840df49fe5f9238c6ea413dbba3d8a788
choprarjun <choprarjun.14@gmail.com>
2023-08-09 20:37:49 -0700

    Adding my written report and the final code from the script.

--- Arjun/exploration.R
+++ Arjun/exploration.R
@@ -6,7 +6,8 @@ library(janitor)
 library(reshape2)
 library(rsample)
 library(parsnip)
-#install.packages("ranger")
+library(ranger)
+library(corrplot)
 data <- readRDS("data/micro_world_139countries.RDS")
 View(data)
 summary(data)
@@ -14,7 +15,7 @@ new_data <- select(data, economy, economycode, regionwb, pop_adult, wpid_random,
                    emp_in, urbanicity_f2f, account, account_fin,
                    account_mob, fin2, fin11_1, fin11a, fin11b, fin11c, fin11d, fin11e, fin11f, fin11g,
                    fin11h)
-data_plot <- select( data, educ, fin5, inc_q )
+data_plot <- select( data, educ, internetaccess, inc_q )
 
 data_plot <- na.omit(data_plot)
 
@@ -31,7 +32,7 @@ data_plot_Cors
 ggplot(data_plot, aes(x= inc_q , y= fin5)) +
   geom_violin() + labs(title = "Income vs. Internet Access")
 
-noNAs <- filter(data, !is.na(inc_q), !is.na(educ), !is.na(fin5))
+noNAs <- filter(data, !is.na(inc_q), !is.na(educ), !is.na(internetaccess))
 
 table1 <- table(noNAs$inc_q, noNAs$educ, noNAs$fin5)
 
@@ -52,20 +53,27 @@ chisq.test(table1)
 chisq.test(table1, noNAs$fin5)
 
 
-chisq.test(noNAs$fin5, noNAs$educ, simulate.p.value = TRUE)
+chisq.test(noNAs$internetaccess, noNAs$educ, simulate.p.value = TRUE)
+
+chisq_educ <- chisq.test(noNAs$internetaccess, noNAs$educ)
+corrplot(chisq_educ$residuals, is.cor = F)
 
 
-chisq.test(noNAs$fin5, noNAs$inc_q)
+chisq.test(noNAs$internetaccess, noNAs$inc_q)
 
+chisq_inc <- chisq.test(noNAs$internetaccess, noNAs$inc_q)
+corrplot(chisq_inc$residuals, is.cor = F)
 set.seed(71723)
 
-noNAs_3 <- select(noNAs, fin5, educ, inc_q)
+##1st ML Model Education, Income level, and Internet Access
+
+noNAs_3 <- select(noNAs, internetaccess, educ, inc_q)
 
 noNAs_factor <- mutate(noNAs_3, educ = as.factor(educ),
                       inc_q = as.factor(inc_q),
-                      fin5 = as.factor(fin5))
+                      internetaccess = as.factor(internetaccess))
 
-class_split <- initial_split(noNAs_factor, prop = 0.75)
+class_split <- initial_split(noNAs_factor, prop = 0.90)
 
 class_train <- training(class_split)
 class_test <- testing(class_split)
@@ -74,11 +82,11 @@ class_test <- testing(class_split)
 forest_class_fit <- rand_forest() |>
   set_engine("ranger") |>
   set_mode("classification") |>
-  fit(fin5 ~ ., data = class_train)
+  fit(internetaccess ~ ., data = class_train)
 
 forest_class_fit$fit
 
-install.packages("Metrics")
+
 library(Metrics)
 class_results <- class_test
 
@@ -87,9 +95,78 @@ class_results$forest_pred <- predict(forest_class_fit, class_test)$.pred_class
 table(class_results$forest_pred)
 
 View(class_results)
-(class_results$fin5 == 1)
+(class_results$internetaccess == 1)
 
-f1(class_results$fin5, class_results$forest_pred)
+f1_score <- f1(class_results$internetaccess, class_results$forest_pred)
 
-?as.factor
 
+
+#2nd ML Model Education and Internet Access
+noNAs_2A <- select(noNAs, internetaccess, educ)
+
+noNAs_factor2A <- mutate(noNAs_2A, educ = as.factor(educ),
+                         internetaccess = as.factor(internetaccess))
+
+class_split2A <- initial_split(noNAs_factor2A, prop = 0.90)
+
+class_train2A <- training(class_split2A)
+class_test2A <- testing(class_split2A)
+
+
+forest_class_fit <- rand_forest() |>
+  set_engine("ranger") |>
+  set_mode("classification") |>
+  fit(internetaccess ~ ., data = class_train2A)
+
+forest_class_fit$fit
+
+
+library(Metrics)
+class_results2A <- class_test2A
+
+class_results2A$forest_pred <- predict(forest_class_fit, class_test2A)$.pred_class
+
+table(class_results2A$forest_pred)
+
+View(class_results2A)
+(class_results$internetaccess == 1)
+
+f2_score <- f1(class_results2A$internetaccess, class_results2A$forest_pred)
+
+#3rd ML Model Education and Internet Access
+noNAs_3A <- select(noNAs, internetaccess, inc_q)
+
+noNAs_factor3A <- mutate(noNAs_3A, educ = as.factor(inc_q),
+                         internetaccess = as.factor(internetaccess))
+
+class_split3A <- initial_split(noNAs_factor3A, prop = 0.90)
+
+class_train3A <- training(class_split3A)
+class_test3A <- testing(class_split3A)
+
+
+forest_class_fit <- rand_forest() |>
+  set_engine("ranger") |>
+  set_mode("classification") |>
+  fit(internetaccess ~ ., data = class_train3A)
+
+forest_class_fit$fit
+
+
+library(Metrics)
+class_results3A <- class_test3A
+
+class_results3A$forest_pred <- predict(forest_class_fit, class_test3A)$.pred_class
+
+table(class_results3A$forest_pred)
+
+View(class_results3A)
+(class_results$internetaccess == 1)
+
+f3_score <- f1(class_results3A$internetaccess, class_results3A$forest_pred)
+
+
+ggplot(data = iris, aes(y= Sepal.Length, x = Species)) +
+  geom_bar(stat = "summary", fun = "mean")
+
+Score <- merge(f1_score, f2_score, f3_score)
